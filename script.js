38e903b42ac2b39b8b7cabf43c5d23b04bc7e09c
Samuel750-ux <samuel.lara.junior@escola.pr.gov.br>
2026-08-27 09:05:50 -0300

    ok

---
+++ script.js
@@ -0,0 +1,13 @@
+// Seleciona o botão e a descrição no HTML usando suas classes
+const botao = document.querySelector('.botao-interativo');
+const descricao = document.querySelector('.descricao');
+
+// Adiciona um evento de clique no botão
+botao.addEventListener('click', () => {
+  // Exibe uma mensagem na tela
+  alert('Obrigado por visitar o site da nossa dupla!');
+  
+  // Altera o texto da descrição ao clicar
+  descricao.textContent = 'Você clicou no botão! Agora você faz parte do nosso universo dos games.';
+  descricao.style.color = '#38bdf8'; // Muda a cor do texto
+});
